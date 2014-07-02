302e9fbb16ed27c675c28302ddd7ff5a5761609d
Christopher Baker <me@christopherbaker.net>
2014-07-02 15:32:48 -0500

    Add additional examples.

---
+++ example_buffered_serial_device/src/ofApp.h
@@ -0,0 +1,68 @@
+// =============================================================================
+//
+// Copyright (c) 2014 Christopher Baker <http://christopherbaker.net>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+// =============================================================================
+
+
+#pragma once
+
+
+#include "ofMain.h"
+#include "ofxSerial.h"
+
+
+class SerialMessage
+{
+public:
+    SerialMessage(): fade(0)
+    {
+    }
+    
+    SerialMessage(const std::string& _message,
+                  const std::string& _exception,
+                  int _fade):
+        message(_message),
+        exception(_exception),
+        fade(_fade)
+    {
+    }
+
+    std::string message;
+    std::string exception;
+    int fade;
+};
+
+
+class ofApp: public ofBaseApp
+{
+public:
+    void setup();
+    void draw();
+    void exit();
+
+    bool onSerialBuffer(const ofx::IO::SerialBufferEventArgs& args);
+    bool onSerialError(const ofx::IO::SerialBufferErrorEventArgs& args);
+
+    ofx::IO::BufferedSerialDevice device;
+
+    std::vector<SerialMessage> serialMessages;
+};
